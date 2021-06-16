bde11c7d6825833cb06f31c8022a7e9245bb59fa
Doo-Dong <ehd6754@gmail.com>
2021-06-16 14:26:18 +0900

    1. 개인정보처리방침 추가 및 HTTP 연결 2. 권한동의화면 교체

--- app/src/main/java/com/corporation/helloworld/Join.java
+++ app/src/main/java/com/corporation/helloworld/Join.java
@@ -3,9 +3,11 @@ package com.corporation.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class Join extends AppCompatActivity {
     Handler handler;
@@ -17,6 +19,7 @@ public class Join extends AppCompatActivity {
         setContentView(R.layout.activity_join);
 
         ImageButton join_btn = findViewById(R.id.join_btn);
+        ImageView join_call_me_2 = findViewById(R.id.join_call_me_2);
 
         join_btn.setOnClickListener(v -> {
             Intent intent = new Intent(getApplicationContext(), Join_input.class);
@@ -24,7 +27,10 @@ public class Join extends AppCompatActivity {
             overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
         });
 
-
+        join_call_me_2.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://sayhello.co.kr/privacy"));
+            startActivity(intent);
+        });
     }
 
 //11
